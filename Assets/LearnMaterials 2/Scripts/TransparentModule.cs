10f45b6d65bd4451d201440a64e578930ff311c1
DippY22 <yulya.gav.200106@gmail.com>
2021-06-22 13:33:18 +0700

    Изменила файл TransparentModule

--- Assets/LearnMaterials 2/Scripts/TransparentModule.cs
+++ Assets/LearnMaterials 2/Scripts/TransparentModule.cs
@@ -6,10 +6,16 @@ using UnityEngine;
 [HelpURL("https://docs.google.com/document/d/1Cmm__cbik5J8aHAI6PPaAUmEMF3wAcNo3rpgzsYPzDM/edit?usp=sharing")]
 public class TransparentModule : MonoBehaviour
 {
+    [SerializeField]
     private float changeSpeed;
 
+    [SerializeField]
     private float defaultAlpha;
+
+    [SerializeField]
     private Material mat;
+
+    [SerializeField]
     private bool toDefault;
 
     private void Start()
@@ -19,6 +25,7 @@ public class TransparentModule : MonoBehaviour
         toDefault = false;
     }
 
+    [ContextMenu("Run script")]
     public void ActivateModule()
     {
         float target = toDefault ? defaultAlpha : 0;
@@ -27,6 +34,7 @@ public class TransparentModule : MonoBehaviour
         toDefault = !toDefault;
     }
 
+    [ContextMenu("ReturnToDefaultState")]
     public void ReturnToDefaultState()
     {
         toDefault = true;
